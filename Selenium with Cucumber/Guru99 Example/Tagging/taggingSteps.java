bad5f6a32b8fa9de45b3bd7135fdc58b182997a7
Umang Bhatia <umangbhatia786@gmail.com>
2018-05-18 19:47:58 +0530

    Create taggingSteps.java

---
+++ Selenium with Cucumber/Guru99 Example/Tagging/taggingSteps.java
@@ -0,0 +1,112 @@
+package stepDefinition;
+
+import org.openqa.selenium.WebDriver;
+import cucumber.api.java.en.Given;
+
+public class taggingSteps {
+	public static WebDriver driver = null;
+	
+	@Given("^This is a Login Test$")
+	public void this_is_a_Login_Test() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is an incorrect Login Test$")
+	public void this_is_an_incorrect_Login_Test() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a contact test case$")
+	public void this_is_a_contact_test_case() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a deal test case$")
+	public void this_is_a_deal_test_case() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a task test case$")
+	public void this_is_a_task_test_case() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a case test case$")
+	public void this_is_a_case_test_case() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^Clicking on left panel links$")
+	public void clicking_on_left_panel_links() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a search deal test case$")
+	public void this_is_a_search_deal_test_case() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a search contact test case$")
+	public void this_is_a_search_contact_test_case() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a search case test case$")
+	public void this_is_a_search_case_test_case() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a search task test case$")
+	public void this_is_a_search_task_test_case() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a search call test case$")
+	public void this_is_a_search_call_test_case() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a search email test case$")
+	public void this_is_a_search_email_test_case() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a search form test case$")
+	public void this_is_a_search_form_test_case() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a search doc test case$")
+	public void this_is_a_search_doc_test_case() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a validation test$")
+	public void this_is_a_validation_test() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("^This is a logout test$")
+	public void this_is_a_logout_test() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	
+}
