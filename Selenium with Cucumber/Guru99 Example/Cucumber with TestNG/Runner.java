16f0f76492b47f34092a40dac05c362878b64a46
Umang Bhatia <umangbhatia786@gmail.com>
2018-05-21 14:28:15 +0530

    Create Runner.java

---
+++ Selenium with Cucumber/Guru99 Example/Cucumber with TestNG/Runner.java
@@ -0,0 +1,44 @@
+
+package TestRunner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+import org.junit.runner.RunWith;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="Features",
+				 glue={"StepDefinition"}, 
+				 plugin={"html:target/cucumber-html-report", "pretty:target/cucumber-pretty.txt",
+						 "usage:target/cucumber-usage.json","junit:target/cucumber-results.xml"},
+				 dryRun = false)
+
+public class Runner {
+	private TestNGCucumberRunner testNGCucumberRunner;
+	 
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() throws Exception {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+ 
+    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+    public void feature(CucumberFeatureWrapper cucumberFeature) {
+        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+    }
+ 
+    @DataProvider
+    public Object[][] features() {
+        return testNGCucumberRunner.provideFeatures();
+    }
+ 
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception {
+        testNGCucumberRunner.finish();
+    }
+	
+}
