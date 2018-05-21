7416422f915eb0f373db9b04f33e9acb4cb90db7
Umang Bhatia <umangbhatia786@gmail.com>
2018-05-21 20:43:53 +0530

    Create TestUtil.java

---
+++ Selenium with Cucumber/Guru99 Example/Cucumber Page Objet Model/Utility/TestUtil.java
@@ -0,0 +1,9 @@
+package utility;
+
+public class TestUtil {
+	public static final int PAGE_LOAD_TIMEOUT = 20;
+	public static final int IMPLICIT_TIMEOUT = 10;
+	public static final String CHROMEDRIVER_PATH = "C:\\Users\\Umang Bhatia\\Documents\\Selenium Drivers\\chrome driver\\chromedriver_win32\\chromedriver.exe";
+	public static final String IEDRIVER_PATH = "C:\\Users\\Umang Bhatia\\Documents\\Selenium Drivers\\IE driver\\IEDriverServer.exe";
+
+}
