c712bea1ea10208c5b2e94659f00af8a05279bf8
Umang Bhatia <umangbhatia786@gmail.com>
2018-05-21 20:37:57 +0530

    Create ContactsPage.java

---
+++ Selenium with Cucumber/Guru99 Example/Cucumber Page Objet Model/Page Factory/ContactsPage.java
@@ -0,0 +1,56 @@
+package pageFactory;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import utility.TestBaseClass;
+
+public class ContactsPage extends TestBaseClass{
+	@FindBy(xpath = "//td[contains(text(),'Contacts')]")
+	WebElement contactsLabel;
+	
+	@FindBy(id="first_name")
+	WebElement firstName;
+	
+	@FindBy(id="surname")
+	WebElement lastName;
+	
+	@FindBy(name="client_lookup")
+	WebElement company;
+	
+	@FindBy(xpath = "//input[@type='submit' and @value='Save']")
+	WebElement saveBtn;
+	
+	
+	
+	// Initializing the Page Objects:
+	public ContactsPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	public boolean verifyContactsLabel(){
+		return contactsLabel.isDisplayed();
+	}
+	
+	
+	public void selectContactsByName(String name){
+		driver.findElement(By.xpath("//a[text()='"+name+"']//parent::td[@class='datalistrow']"
+				+ "//preceding-sibling::td[@class='datalistrow']//input[@name='contact_id']")).click();
+	}
+	
+	
+	public void createNewContact(String title, String ftName, String ltName, String comp){
+		Select select = new Select(driver.findElement(By.name("title")));
+		select.selectByVisibleText(title);
+		
+		firstName.sendKeys(ftName);
+		lastName.sendKeys(ltName);
+		company.sendKeys(comp);
+		saveBtn.click();
+		
+	}
+}
